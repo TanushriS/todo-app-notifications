4be131237ad1ce25c97a2df1cd54d1029513354f
Tanushri Sukhwal <139938767+TanushriS@users.noreply.github.com>
2025-07-05 22:11:15 +0530

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -4,7 +4,7 @@ import { cookies } from "next/headers"
 export async function createClient() {
   const cookieStore = await cookies()
 
-  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
+  return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL+"https://prcomnldozwjodkuokaj.supabase.co", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InByY29tbmxkb3p3am9ka3Vva2FqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MjI3NTYsImV4cCI6MjA2NzI5ODc1Nn0._19Qy9kAApEVG3D4Efd6DF9gjHlbP_8caL3DLguSkNE", {
     cookies: {
       getAll() {
         return cookieStore.getAll()
@@ -13,7 +13,7 @@ export async function createClient() {
         try {
           cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options))
         } catch {
-          // The `setAll` method was called from a Server Component.
+          // The setAll method was called from a Server Component.
           // This can be ignored if you have middleware refreshing
           // user sessions.
         }
